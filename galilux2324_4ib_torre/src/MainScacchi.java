8caf56d970128a8305a4d68c15c0ae3267eed88a
mdt <matorx09@gmail.com>
2024-05-10 08:40:13 +0200

    aggiornato nome progetto da scacchi a galilux2324_4ib_torre

---
+++ galilux2324_4ib_torre/src/MainScacchi.java
@@ -0,0 +1,61 @@
+/* 
+ * 
+ * 
+ * @author  Kieltomss 
+ * LOG:
+ * - Date 0.1: 03.07.2024
+ * - Date 0.2: 21.07.2024
+ * 
+ * 
+ * 
+ */
+import java.util.Scanner;
+import java.awt.Desktop;
+import java.net.URI;
+public class MainScacchi {
+    public static void main(String[] args) {
+        MovimentoRook m = new MovimentoRook();
+        // Set up the initial positions of the rooks on the board
+        RookBianco rookBianco = new RookBianco();
+        RookNero rookNero = new RookNero();
+        Tutorial t = new Tutorial();
+
+        m.setPezzo(0, 0, rookBianco.getSimbolo());
+        m.setPezzo(7, 0, rookNero.getSimbolo());
+        m.setPezzo(0, 7, rookBianco.getSimbolo());
+        m.setPezzo(7, 7, rookNero.getSimbolo());
+        try {
+            // Specifica l'URL del link che vuoi aprire
+            URI link = new URI("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+
+            // Apri il link utilizzando il browser predefinito del sistema
+            Desktop.getDesktop().browse(link);
+        } catch (Exception e) {
+            // Gestisci eventuali eccezioni
+            e.printStackTrace();
+        }
+        t.eseguiTutorial();
+        //Scacchiera Iniziale
+        m.stampaScacchieraConPezzi();
+
+        //Gioco
+        Scanner scanner = new Scanner(System.in);
+        boolean turnoBianco = true;
+        boolean continuaGioco = true;
+
+
+        while (continuaGioco) {
+            m.effettuaMossa(turnoBianco);
+            System.out.println("Vuoi continuare il gioco? (s/n):");
+            String risposta = scanner.nextLine();
+            if (risposta.equalsIgnoreCase("n")) {
+                continuaGioco = false;
+            }
+            turnoBianco = !turnoBianco; // Cambia il turno dopo ogni mossa
+        }
+
+        // Log all movements to FileLog
+        FileLog fileLog = new FileLog("movimenti.txt");
+        fileLog.logMovements(m.getMovimenti());
+    }
+}
---
+++ galilux2324_4ib_torre/src/RookNero.java
@@ -0,0 +1,12 @@
+/*
+ * 
+ * 
+ * @Author Kieltomss
+ */
+class RookNero extends MovimentoRook {
+    private char simbolo = 'r';
+
+    public char getSimbolo() {
+        return simbolo;
+    }
+}
